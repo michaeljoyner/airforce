110a5738031935f1bdc401e344fcc5437a237ff9
Michael Joyner <joyner.michael@gmail.com>
2020-05-17 12:20:03 +0800

    wip

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,17 @@
-module.exports = {
-  pwa: {
-    name: "Taichung AQI",
-    themeColor: "#f6e05e",
-    msTileColor: "#000000",
-    appleMobileWebAppCapable: "yes",
-    appleMobileWebAppStatusBarStyle: "black",
+// module.exports = {
+//   pwa: {
+//     name: "Taichung AQI",
+//     themeColor: "#f6e05e",
+//     msTileColor: "#000000",
+//     appleMobileWebAppCapable: "yes",
+//     appleMobileWebAppStatusBarStyle: "black",
 
-    // configure the workbox plugin
-    workboxPluginMode: "InjectManifest",
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: "/sw.js",
-      // ...other Workbox options...
-    },
-  },
-};
+//     // configure the workbox plugin
+//     workboxPluginMode: "InjectManifest",
+//     workboxOptions: {
+//       // swSrc is required in InjectManifest mode.
+//       swSrc: "/service-worker.js",
+//       // ...other Workbox options...
+//     },
+//   },
+// };
